91edeac20cbba344bdea7c9ab0e53f7daecc23c6
Rikad Fauzi Alawi <rikadfauzi@gmail.com>
2024-10-15 04:43:39 +0000

    freecodecamp challenge

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+const urlStore = ['https://google.com']
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +23,33 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url
+  const host = new URL(url).host
+  console.log(host)
+
+  dns.lookup(host, (err) => {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      urlStore.push(url)
+      res.json({ original_url : url, short_url : urlStore.length-1 });
+    }
+  })
+});
+
+app.get('/api/shorturl/:url', function(req, res) {
+  const id = parseInt(req.params.url)
+  const url = urlStore[id]
+
+  if (url) {
+    res.redirect(url)
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
